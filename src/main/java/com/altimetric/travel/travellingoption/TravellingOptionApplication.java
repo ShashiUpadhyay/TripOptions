a4c58c635319a5009d9b747e0d2fc2af916f5514
NagendraLimbu <nagendra.limbu@gmail.com>
2018-08-24 09:29:42 -0500

    project created

---
+++ src/main/java/com/altimetric/travel/travellingoption/TravellingOptionApplication.java
@@ -0,0 +1,12 @@
+package com.altimetric.travel.travellingoption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravellingOptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravellingOptionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/altimetric/travel/travellingoption/TravellingOptionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.altimetric.travel.travellingoption;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TravellingOptionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
